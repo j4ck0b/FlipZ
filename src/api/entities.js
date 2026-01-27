34624a83cde8ea85d58f3c484a016470cefd291c
j4ck0b <75251166+j4ck0b@users.noreply.github.com>
2026-01-27 02:07:02 +0100

    Update entities.js

--- src/api/entities.js
+++ src/api/entities.js
@@ -1,9 +1,5 @@
 import { base44 } from './base44Client';
 
-
-export const Query = base44.entities.Query;
-
-
-
-// auth sdk:
+// Eksportujemy Query i User, żeby reszta aplikacji ich używała
+export const Query = base44.entities; 
 export const User = base44.auth;
