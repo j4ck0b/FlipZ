bb450178b5faca9fae1e27cff0d0c13e93734da9
j4ck0b <75251166+j4ck0b@users.noreply.github.com>
2026-01-27 02:17:15 +0100

    Update entities.js

--- src/api/entities.js
+++ src/api/entities.js
@@ -1,5 +1,8 @@
 import { base44 } from './base44Client';
 
-// Eksportujemy Query i User, żeby reszta aplikacji ich używała
-export const Query = base44.entities; 
+// Eksportujemy narzędzia tak, aby reszta komponentów (np. Home.jsx) nie widziała różnicy
+export const Query = base44.entities;
 export const User = base44.auth;
+
+// Dodatkowe skróty dla integracji, jeśli Twoja strona ich szuka
+export const Core = base44.functions;
