dae9d94a0696495a1cb4753f3ad73ee756e7d4b6
j4ck0b <75251166+j4ck0b@users.noreply.github.com>
2026-02-04 01:01:38 +0100

    Force dark theme and override light styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
 import '@/index.css'
 
+document.documentElement.classList.add('dark');
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <App />
@@ -17,6 +19,5 @@ if (import.meta.hot) {
     window.parent?.postMessage({ type: 'sandbox:afterUpdate' }, '*');
   });
 }
-
 
 
